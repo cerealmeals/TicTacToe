0f3bd14e54eb6e2949bdd5a350c0ea8e4bf204be
sam <samnygard@gmail.com>
2024-06-14 09:14:15 -0700

    started the logic and display, added coments to explain functions

--- functions.js
+++ functions.js
@@ -1,39 +1,90 @@
 const Board = (function() {
-    let size = 3;
-    const board = [];
-    for(let i = 0; i < size; i++){
-        board[i] = []
-        for(let j = 0; j < size; j++)
-            board[i].push("");
-    }
+    let size = 3; // number of rows and columns on the board
+    const board = []; // stores the values in the board
+    makeBoard(); // make an initial board
 
-    const clearboard = function (){
-        for(let i = 0; i < size; i++){
-            board[i] = []
-            for(let j = 0; j < size; j++)
-                board[i].push("");
+    // make the inital array with no characters
+    const makeBoard = function (){
+        for(let i = 0; i < size*size; i++){
+            board.push("");
         }
     }
 
-    const setSize = function(n){
-        size = n;
-        clearboard();
+    // clear the array to set up for another game
+    const clearboard = function (){
+        for(let i = 0; i < size*size; i++){
+            board[i] = "";
+        }
     }
 
+    // set a new size for a new game
+    const setSize = function(n){
+        board.splice(0, board.length);
+        size = n;
+        makeBoard();
+    }
+
+    // getter for size
     const getSize = () => size;
-    return {board, clearboard, setSize, getSize};
+    return {board, clearboard, setSize, getSize, makeBoard};
 })();
 
-let arr = [""];
-let x = 0;
-console.log(arr[0]);
-console.log(x);
+const display = (function(){
+
+    // creates the elements to display the board to the screen
+    const createBoard = function (){
+        const root = document.querySelector(".board");
+        for (let i = 0; i < Board.getSize()**2; i++){
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.addEventListener("click", function(){
+                square.textContent = Logic.place();
+                Logic
+                Logic.checkWinner(Board);
+            })
+            square.textContent = Board.board[i];
+            root.appendChild(square);
+        }
+    }
+
+    // clear the elements on the screen to make a new board
+    const clearboard = function (){
+
+    }
+    return {createBoard, clearboard}
+})();
+
+const Logic = (function(){
+    let Xturn = true; //on true it is X's turn on false it is O's turn 
+
+    //getting for who's turn it is, returns a string either X or O
+    const getTurn = function(){
+        if(Xturn){
+            return "X";
+        }
+        else{
+            return "O";
+        }
+    }
+
+    // returns the turn and swaps who's turn it is
+    const place = function (){
+        if(Xturn){
+            Xturn = false;
+            return "X";
+        }
+        else{
+            Xturn = true;
+            return "O";
+        }
+    }
+
+    // checks if the current board stat has a winner
+    const checkWinner = function(){
+        console.log("no logic implemented");
+    }
+    return {getTurn, checkWinner, place}
+})();
+
+
 console.log(Board.board);
-
-arr[0] = "x";
-x = 1;
-Board.board[0][0] = "x";
-
-console.log(arr[0])
-console.log(x)
-console.log(Board.board)
